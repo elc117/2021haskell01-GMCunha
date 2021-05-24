507907b858650e293ea4b8af577c1db5bf12ff8f
GMCunha <>
2021-05-24 15:09:27 +0000

    mais exercicios concluidos

--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -41,6 +41,9 @@ isVowel2 c = elem c "aeiouAEIOU"
 isLongWord :: String -> Bool
 isLongWord s = length s > 10
 
+isEvenBetter  :: Int -> Bool
+isEvenBetter n = mod n 2 == 0
+
 -- aplica html em uma lista
 itemize :: [String] -> [String]
 itemize s = map htmlItem s
@@ -56,3 +59,15 @@ onlyElderly ages = filter isElderly ages
 -- remove palavras pequenas de uma lista
 onlyLongWords :: [String] -> [String]
 onlyLongWords strs = filter isLongWord strs
+
+-- retorna numeros pares de uma lista
+onlyEven :: [Int] -> [Int]
+onlyEven strs = filter isEvenBetter strs
+
+-- funcao auxiliar para onlyBetween60and80
+between60and80 :: Int -> Bool
+between60and80 n = (n >= 60) && (n <= 80)
+
+-- remove numeros que nao estiverem entre 60 e 80 em uma lista
+onlyBetween60and80 :: [Int] -> [Int]
+onlyBetween60and80 strs = filter between60and80 strs
