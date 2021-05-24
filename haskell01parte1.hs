d42bad47f6374cd3baf76c225f0fe9abd7f58ce7
GMCunha <>
2021-05-24 14:09:23 +0000

    parte 1 concluida, fazendo parte 2

--- haskell01parte1.hs
+++ haskell01parte1.hs
@@ -1,2 +1,48 @@
 -- Prática 01 de Haskell - Parte 1
--- Nome: 
+-- Nome: Guilherme Medeiros da Cunha
+
+-- calcula a soma dos quadrados de x e y
+sumSquares :: Int -> Int -> Int
+sumSquares x y = x^2 + y^2
+
+-- calcula a area de um circulo
+circleArea :: Float -> Float
+circleArea r = pi*r^2
+
+-- calcula a idade aproximada
+age :: Int -> Int -> Int
+age n a = a-n
+
+-- verifica se idade eh maior que 65 anos
+isElderly :: Int -> Bool
+isElderly n = n>65
+
+-- adiciona <li> no inicio e <\li> no final de uma string
+htmlItem :: String -> String
+htmlItem s = "<li>" ++ s ++ "<\\li>"
+
+-- verifica se string inicia com A
+startsWithA :: String -> Bool
+startsWithA s = (head s) == 'A'
+
+-- verifica se string termina com r
+isVerb :: String -> Bool
+isVerb s = (last s) == 'r'
+
+-- verifica se caractere eh uma vogal minuscula
+isVowel :: Char -> Bool
+isVowel c
+  | c == 'a' = True
+  | c == 'e' = True
+  | c == 'i' = True
+  | c == 'o' = True
+  | c == 'u' = True
+  | otherwise = False
+
+-- verifica se duas listas tem o mesmo primeiro elemento
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads s1 s2 = (head s1) == (head s2)
+
+-- isVowel2
+isVowel2 :: Char -> Bool
+isVowel2 c = elem c "aeiouAEIOU"
--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,2 +1,58 @@
 -- Prática 01 de Haskell - Parte 2
--- Nome: 
+-- Nome: Guilherme Medeiros da Cunha
+
+-- funcoes para implementacao
+sumSquares :: Int -> Int -> Int
+sumSquares x y = x^2 + y^2
+
+circleArea :: Float -> Float
+circleArea r = pi*r^2
+
+age :: Int -> Int -> Int
+age n a = a-n
+
+isElderly :: Int -> Bool
+isElderly n = n>65
+
+htmlItem :: String -> String
+htmlItem s = "<li>" ++ s ++ "<\\li>"
+
+startsWithA :: String -> Bool
+startsWithA s = (head s) == 'A'
+
+isVerb :: String -> Bool
+isVerb s = (last s) == 'r'
+
+isVowel :: Char -> Bool
+isVowel c
+  | c == 'a' = True
+  | c == 'e' = True
+  | c == 'i' = True
+  | c == 'o' = True
+  | c == 'u' = True
+  | otherwise = False
+
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads s1 s2 = (head s1) == (head s2)
+
+isVowel2 :: Char -> Bool
+isVowel2 c = elem c "aeiouAEIOU"
+
+isLongWord :: String -> Bool
+isLongWord s = length s > 10
+
+-- aplica html em uma lista
+itemize :: [String] -> [String]
+itemize s = map htmlItem s
+
+-- remove consoantes de uma String
+onlyVowels :: String -> String
+onlyVowels s = filter isVowel2 s
+
+-- remove idades menores ou iguais a 65 de uma lista
+onlyElderly :: [Int] -> [Int]
+onlyElderly ages = filter isElderly ages
+
+-- remove palavras pequenas de uma lista
+onlyLongWords :: [String] -> [String]
+onlyLongWords strs = filter isLongWord strs
